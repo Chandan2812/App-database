bfbf94db984bafd9a023b261b177dd2cccb4f506
Shubham9049 <shubham.rajveer19@gmail.com>
2025-02-07 15:54:57 +0530

    updated clerkid

--- model/user.model.js
+++ model/user.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    clerkId: { type: String, unique: true, sparse: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: false  },
